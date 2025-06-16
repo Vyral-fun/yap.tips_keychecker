41e2aaa294e9db5242d36b5343fda09f909b765c
AdityaCubicles45 <aditya.singh456m@gmail.com>
2025-06-16 17:14:43 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jersey15.variable} ${jersey20.variable} antialiased`}
       >
